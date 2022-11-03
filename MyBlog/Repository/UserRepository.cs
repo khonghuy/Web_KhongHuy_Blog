f157629e7569ec1755690db09f8d6d47433bcb13
khonghuy <75528623+khonghuy@users.noreply.github.com>
2022-11-03 16:37:53 +0700

    Update UserRepository.cs

--- MyBlog/Repository/UserRepository.cs
+++ MyBlog/Repository/UserRepository.cs
@@ -41,5 +41,39 @@ namespace MyBlog.Repository
             }).ToListAsync();
             
         }
+        public async Task<bool> DeleteUser(Guid Id){
+            var user = await _context.Users.FirstOrDefaultAsync(user => user.Id == Id);
+            if(user == null){
+                return false;
+               
+            };
+             _context.Users.Remove(user);
+             await _context.SaveChangesAsync();
+             return true;
+        }   
+
+        public async Task<UserDto>  EditUser (Guid Id, User user)
+        {
+            var userExist = await _context.Users.FirstOrDefaultAsync(user => user.Id == Id);
+            if(userExist == null){
+                return null;
+            };
+            userExist.DisplayName = user.DisplayName;
+            userExist.Email = user.Email;
+            userExist.Phone = user.Phone;
+            userExist.DateOfBirth = user.DateOfBirth;
+            userExist.Address = user.Address;
+            await _context.SaveChangesAsync();
+
+            return new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Phone = user.Phone,
+                ID = user.Id,
+                DateOfBirth = user.DateOfBirth,
+                Address = user.Address,
+            };
+        }
     }
 }
